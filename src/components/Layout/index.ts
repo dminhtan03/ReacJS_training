ca60130e203b50a60ea67dabbfac9346dd6676bf
DINH MINH TAN <dinhminhtan112@lgcns.com>
2025-08-12 14:16:25 +0700

    clean code

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,6 +1,5 @@
 // ===== EXPORT TẤT CẢ LAYOUT COMPONENTS =====
 
 export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
+
 export { default as Sidebar } from "./Sidebar";
-export { default as MainLayout } from "./MainLayout";
